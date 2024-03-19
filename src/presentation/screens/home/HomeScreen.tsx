aa28fda218d1451b55f56d97ab0ecd265c828e48
Sebas19ujcv <jose.castro2@ujcv.edu.hn>
2024-03-18 20:58:41 -0600

    Se agrega el proyecto

---
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import { Text, requireNativeComponent } from "react-native";
+
+export const HomeScreen = () => {
+    return (
+        <view>
+<Text>HomeScreen</Text>
+
+        </view>
+    )
+}
+
+requireNativeComponent
